828a43a4d69b6a65f72e0f00d7d02e1f16982374
jon <proto@mindsign.net>
2017-05-04 08:38:52 -0700

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -37,7 +37,7 @@ config :phoenix, :stacktrace_depth, 20
 config :soroban, Soroban.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "r2d297",
+  password: "",
   database: "soroban_dev",
   hostname: "localhost",
   pool_size: 10
