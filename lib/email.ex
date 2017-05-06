d68f87aa0b6fd550166974086eb19a3211667483
jon <proto@mindsign.net>
2017-05-06 12:35:49 -0700

    import

---
+++ lib/email.ex
@@ -0,0 +1,50 @@
+defmodule Soroban.Email do
+  use Bamboo.Phoenix, view: Soroban.EmailView
+
+  alias Soroban.Mailer
+
+  @moduledoc """
+  A module for sending emails
+
+  Our emails can be sent as pure text, HTML or even as an application view, but
+  to keep it simple we will use text only in the examples below.
+  Check the Phoenix Framework docs (http://www.phoenixframework.org/docs/sending-email)
+  for more details on how to send other types of emails
+  """
+
+  @doc """
+  An email with a confirmation link in it.
+  """
+  def ask_confirm(email, link) do
+    new_email()
+    |> to(email)
+    |> from("welcome@example.com")
+    |> subject("Confirm your account - Welcome Soroban")
+    |> text_body("Confirm your Soroban email here http://www.example.com/sessions/confirm_email?#{link}")
+    |> Mailer.deliver_now
+  end
+
+  @doc """
+  An with a link to reset the password.
+  """
+  def ask_reset(email, link) do
+    new_email()
+    |> to(email)
+    |> from("welcome@example.com")
+    |> subject("Reset your password - Soroban")
+    |> text_body("Reset your password at http://www.example.com/password_resets/edit?#{link}")
+    |> Mailer.deliver_now
+  end
+
+  @doc """
+  An email acknowledging that the account has been successfully confirmed.
+  """
+  def receipt_confirm(email) do
+    new_email()
+    |> to(email)
+    |> from("welcome@example.com")
+    |> subject("Confirmed account - Soroban")
+    |> text_body("Your account has been confirmed!")
+    |> Mailer.deliver_now
+  end
+end
