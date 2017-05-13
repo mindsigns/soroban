31653c05db706a06fbc36df63b1dc2c339b58778
jon <proto@mindsign.net>
2017-05-12 21:21:45 -0700

    add bamboo templates

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,8 @@ config :soroban, Soroban.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex)$},
+      ~r{deps/bamboo/lib/bamboo/plug/.*(eex)$}
     ]
   ]
 
