420e9ea0f1a55a522b334727d14253146354b6a1
jon <proto@mindsign.net>
2017-05-23 12:48:36 -0700

    remove sentinel

--- web/router.ex
+++ web/router.ex
@@ -1,6 +1,5 @@
 defmodule Soroban.Router do
   use Soroban.Web, :router
-  require Sentinel
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -10,45 +9,12 @@ defmodule Soroban.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
-  pipeline :sentinel_ueberauth do
-    plug :accepts, ["html", "json"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :put_secure_browser_headers
-  end
-
-  scope "/" do
-    pipe_through :sentinel_ueberauth
-    Sentinel.mount_ueberauth
-  end
-
-  scope "/" do
+  scope "/", Soroban do
     pipe_through :browser
-    Sentinel.mount_html
-  end
-
-  scope "/api", as: :api do
-    pipe_through :api
-    Sentinel.mount_api
-  end
-
-  scope "/", Soroban do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
-  end
-
-  scope "/", Soroban do
-    pipe_through :sentinel_ueberauth
     get "/", PageController, :index
     resources "/jobs", JobController
     resources "/service", ServiceController
     resources "/jobtypes", JobtypeController
   end
 
-    forward "/sent_emails", Bamboo.EmailPreviewPlug
 end
