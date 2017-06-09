8f03c9a037b20c11eb85e0811a92818f3273e0b2
jon <proto@mindsign.net>
2017-06-09 11:35:42 -0700

    Order by name

--- web/controllers/job_controller.ex
+++ web/controllers/job_controller.ex
@@ -86,7 +86,7 @@ defmodule Soroban.JobController do
    end
 
    defp load_clients(conn, _) do
-      clients = Repo.all from c in Client, select: {c.name, c.id}
+      clients = Repo.all from c in Client, order_by: c.name, select: {c.name, c.id}
       assign(conn, :clients, clients)
    end
 
