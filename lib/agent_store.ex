64acbf13553f2f302c6f77817c26e1b76d153227
jon <proto@mindsign.net>
2017-07-10 19:45:13 -0700

    import Slingbag functions for caching filenames, ect

---
+++ lib/agent_store.ex
@@ -0,0 +1,25 @@
+defmodule Slingbag do
+
+  def start_link do
+    Agent.start_link(fn -> [] end, name: __MODULE__)
+  end
+
+  def add_filename(filename) do
+    Agent.update(__MODULE__,
+                 fn list ->
+                   [filename|list]
+                 end)
+  end
+
+  def filenames do
+    Agent.get(__MODULE__, fn list -> list end)
+  end
+
+  def empty do
+    Agent.update(__MODULE__,
+                 fn list ->
+                   []
+                 end)
+  end
+
+end
