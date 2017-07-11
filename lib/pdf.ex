2b9d5b5b7d9d53bbc5572cb17194cfa6541f9bff
jon <proto@mindsign.net>
2017-07-10 19:44:03 -0700

    add functions for batch pdf generation and sendfile

--- lib/pdf.ex
+++ lib/pdf.ex
@@ -11,18 +11,48 @@ defmodule Soroban.Pdf do
   def invoice_send_pdf(conn, html, client, invoicenum) do
     {:ok, filename} = PdfGenerator.generate(html, delete_temporary: true)
 
-    clientname = String.replace(client, ~r/[," "&']/, "")
-    savefile = Enum.join([clientname, "_", invoicenum, ".pdf"])
+    savefile = create_file_name(client, invoicenum)   
+  
+    send_a_file(conn, filename, savefile)
+  end
 
+  def invoice_batch_zip(html, invoicenum, client) do
+    {:ok, filename} = PdfGenerator.generate(html, delete_temporary: true)
+    savefile = create_file_name(client, invoicenum)   
+    File.rename(filename, Enum.join(["/home/jon/src/Elixir/soroban/priv/static/pdf/", savefile]))
+    Slingbag.add_filename(String.to_char_list(savefile))
+  end
+
+  def invoice_send_zip(conn, invoicenum) do
+    files = Slingbag.filenames
+    zipfilename = Enum.join([invoicenum, ".zip"])
+    {:ok, filename} = :zip.create(zipfilename, Slingbag.filenames, [cwd: '/home/jon/src/Elixir/soroban/priv/static/pdf'])
+    send_a_file(conn, filename, filename)
     conn
-    |> put_resp_content_type("application/pdf")
-    |> put_resp_header("content-disposition", "attachment; filename=#{savefile}")
-    |> send_file(200, filename)
+  end
 
-    case File.exists?(filename) do
-      true -> File.rm(filename)
+  #
+  # Private functions
+  #
+  defp create_file_name(client, invoicenum) do
+    clientname = String.replace(client, ~r/[," "&']/, "")
+    Enum.join([invoicenum, "_", clientname, ".pdf"])
+  end
+
+  defp clean_up(_conn, filename) do
+    file_to_del = Enum.join(["/home/jon/src/Elixir/soroban/priv/static/pdf/", filename])
+    case File.exists?(file_to_del) do
+      true -> File.rm(file_to_del)
       _ -> "no file to remove"
     end
+  end
+
+  defp send_a_file(conn, filename, savefile) do
+    conn
+      |> put_resp_content_type("application/pdf")
+      |> put_resp_header("content-disposition", "attachment; filename=#{savefile}")
+      |> send_file(200, filename)
+      |> clean_up(filename)
   end
 
 end
