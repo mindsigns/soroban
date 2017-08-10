ff80efb1d056bd5b838e9367317143eda27541e1
jon <jon@mindsign.net>
2017-08-10 07:09:12 -0700

    fix spacing.

--- lib/invoiceutils.ex
+++ lib/invoiceutils.ex
@@ -4,6 +4,7 @@ defmodule Soroban.InvoiceUtils do
   """
 
   import Ecto.Query
+  use Drab.Commander
 
   alias Soroban.{Job, Invoice, Setting, Repo}
   alias Soroban.Pdf
@@ -29,8 +30,8 @@ defmodule Soroban.InvoiceUtils do
     Repo.update!(changeset)
 
   case pdf_tf do
-    true -> Pdf.to_pdf(invoice, jobs, total, company)
-    false  -> "skipping pdf generation"
+    true  -> Pdf.to_pdf(invoice, jobs, total, company)
+    false -> "skipping pdf generation"
   end
 
   {invoice, jobs, total, company}
@@ -43,16 +44,21 @@ defmodule Soroban.InvoiceUtils do
     total
   end
 
-  def batch_job(_conn, clients, params) do
+  def batch_job(socket, clients, params) do
+    #def batch_job(_conn, clients, params) do
     %{"invoice" => %{"date" => date, "end" => end_date, "start" => start_date, "number" => number}} =  params
+    #%{"date" => date, "stop" => end_date, "start" => start_date, "number" => number} =  params
 
     for c <- clients do
-      case Enum.count(Repo.all from c in Soroban.Client, join: j in Soroban.Job, where: j.client_id == ^c) do
+    case Enum.count(Repo.all from c in Soroban.Client, join: j in Soroban.Job, where: j.client_id == ^c) do
         0 ->  "No Jobs"
         _ ->  invoice_id = new_invoice(c, date, end_date, start_date, number)
               generate(invoice_id, true)
+              client = Repo.get(Soroban.Client, c)
+              poke socket, text: Enum.join(["Invoicing for : ", client.name])
       end
     end
+    poke socket, text: "Done generating invoices! <a href='/invoices'>View</a>"
   end
 
   def batch_email(invoice_id_list) do
@@ -61,12 +67,12 @@ defmodule Soroban.InvoiceUtils do
       case is_nil(invoice.client.email) do
         false -> Soroban.Email.invoice_html_email(invoice.client.email, invoice, jobs, total, company)
                   |> Soroban.Mailer.deliver_later
-        true -> Slingbag.add(invoice.client.name)
+        true  -> Slingbag.add(invoice.client.name)
       end
     end
   end
 
-  defp new_invoice(id, date, end_date, start_date, number) do
+  def new_invoice(id, date, end_date, start_date, number) do
     changeset = Invoice.changeset(%Invoice{}, %{"client_id" => id,
                                     "number" => number,
                                     "date" => date,
