b3974f5ad9222f38d4db80270b7b167944a9594d
jon <jon@mindsign.net>
2017-08-11 08:23:48 -0700

    wait for 1 second to give InvoiceUtils enough time to gen at least one

--- web/controllers/batch_controller.ex
+++ web/controllers/batch_controller.ex
@@ -54,6 +54,7 @@ defmodule Soroban.BatchController do
 
     Task.async(InvoiceUtils, :batch_job, [conn, clients, params])
 
+    :timer.sleep(1000)
     conn
       |> put_flash(:info, "Generating all invoices, this will take a bit.")
       |> redirect(to: invoice_path(conn, :index))
