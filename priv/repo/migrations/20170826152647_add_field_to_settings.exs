91113b9960f637365ad066c73db86dd99b1a0039
jon <jon@mindsign.net>
2017-08-26 11:45:37 -0700

    add invoice_image to settings model

--- priv/repo/migrations/20170826152647_add_field_to_settings.exs
+++
@@ -1,9 +0,0 @@
-defmodule Soroban.Repo.Migrations.AddFieldToSettings do
-  use Ecto.Migration
-
-  def change do
-    alter table(:settings) do
-      add :invoice_image, :string
-    end
-  end
-end
