cb4840416152154a0a7a47623de6da290bc1a4f4
jon <jon@mindsign.net>
2017-08-29 13:58:30 -0700

    add drab templates

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,7 @@ config :soroban, Soroban.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$},
+      ~r{web/templates/.*(eex|drab)$},
       ~r{deps/bamboo/lib/bamboo/plug/.*(eex)$}
     ]
   ]
