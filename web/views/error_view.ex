0d796e54d2c225b251134120c6d90314ffdba134
jon <jon@mindsign.net>
2017-09-18 19:07:54 -0700

    add custom error messages

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,13 +1,14 @@
 defmodule Soroban.ErrorView do
   use Soroban.Web, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
+  #def render("404.html", _assigns) do
+  ##"Page not found"
+    #  render("not_found.html", %{})
+    #end
 
-  def render("500.html", _assigns) do
-    "Internal server error"
-  end
+    #def render("500.html", _assigns) do
+    #"Internal server error"
+    #end
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
