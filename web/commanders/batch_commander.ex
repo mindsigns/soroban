5a02f23b62a35979f83cf0bda0f397e3beeae847
jon <proto@mindsign.net>
2018-03-03 19:53:26 -0600

    update Drab to 0.7.1 and refactor accordingly

--- web/commanders/batch_commander.ex
+++ web/commanders/batch_commander.ex
@@ -8,13 +8,14 @@ defmodule Soroban.BatchCommander do
   alias Soroban.{Repo, Client, InvoiceUtils}
 
   def invoice(socket, params) do
-    %{"invoice" => %{"date" => _, "end" => _, "start" => _, "number" => number}} =  params.params
+    number = socket |> Drab.Query.select(:val, from: "input[id=invoice_number]")
 
     if number == "" do
       poke socket, text: "Enter an Invoice Number"
     else
+      newparams =  repack(params.params)
       clients = Repo.all from c in Client, select: c.id
-      Task.async(InvoiceUtils, :batch_job, [socket, clients, params.params])
+      Task.async(InvoiceUtils, :batch_job, [socket, clients, newparams])
     end
   end
 
@@ -22,4 +23,21 @@ defmodule Soroban.BatchCommander do
     poke socket, text: "sup"
   end
 
+  def repack(params) do
+    repacked = %{
+				"invoice" => %{
+        			"date" => %{"day" => Map.get(params, "invoice[date][day]"),
+        						"month" => Map.get(params, "invoice[date][month]"),
+        						"year" => Map.get(params, "invoice[date][year]")},
+        			"end"  => %{"day" => Map.get(params, "invoice[end][day]"),
+        						"month" => Map.get(params, "invoice[end][month]"),
+        						"year" => Map.get(params, "invoice[end][year]")},
+        			"number" => Map.get(params, "invoice[number]"),
+        			"start" => %{"day" => Map.get(params, "invoice[start][day]"),
+        						 "month" => Map.get(params, "invoice[start][month]"),
+        						 "year" => Map.get(params, "invoice[start][year]")}
+    }
+  }
+	repacked
+  end
 end
