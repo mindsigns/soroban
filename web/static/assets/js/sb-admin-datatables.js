bde1e53f46491ce87e544b226afd943f34ab1a11
jon <proto@mindsign.net>
2018-11-12 19:35:56 -0600

    remove mouse click search on datatables

--- web/static/assets/js/sb-admin-datatables.js
+++ web/static/assets/js/sb-admin-datatables.js
@@ -19,11 +19,6 @@ $(document).ready(function() {
     $('#dataTable').DataTable( {
                     dom: 'Bfrltip',
                     lengthMenu: [[25, 50, 100, -1], [25, 50, 100, "All"]],
-                    initComplete: function () {
-                        var api = this.api();
-                        api.$('td').click( function () {
-                        api.search( this.innerHTML ).draw();
-                    } ); },
 		            deferRender: true,
                     buttons: [ 'csv', 'excel', 'print' ]
                     } );
