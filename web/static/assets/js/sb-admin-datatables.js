5db0325e970f214f0174b585b402251bd8e9dc15
jon <proto@mindsign.net>
2018-11-30 09:25:38 -0600

    button alignment

--- web/static/assets/js/sb-admin-datatables.js
+++ web/static/assets/js/sb-admin-datatables.js
@@ -17,7 +17,8 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $('#dataTable').DataTable( {
-                    dom: 'Bfrltip',
+                    //dom: 'Bfrltip',
+                    dom : "<'row'<'col-sm-6'B><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-6'i><'col-sm-6'p>>", 
                     lengthMenu: [[25, 50, 100, -1], [25, 50, 100, "All"]],
 		            deferRender: true,
                     buttons: [ 'csv', 'excel', 'print' ]
