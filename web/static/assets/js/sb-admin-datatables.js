7b2308e73f46da400c42773ff6336919b6f25865
jon <proto@mindsign.net>
2018-12-01 07:55:08 -0600

    DT updates

--- web/static/assets/js/sb-admin-datatables.js
+++ web/static/assets/js/sb-admin-datatables.js
@@ -26,4 +26,14 @@ $(document).ready(function() {
                     } );
 } );
 
+$(document).ready(function() {
+    $('#dataTableJob').DataTable( {
+                    //dom: 'Bfrltip',
+                    dom : "<'row'<'col-sm-6'B><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-6'il><'col-sm-6'p>>", 
+                    lengthMenu: [[25, 50, 100, -1], [25, 50, 100, "All"]],
+		            deferRender: true,
+                    "order": [[ 1, "desc" ]],
+                    buttons: [ 'csv', 'excel', 'print' ]
+                    } );
+} );
 
